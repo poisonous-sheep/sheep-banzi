ce4cfe354b73fd456dcfbc65e204e7611fad5cb1
poisonous-sheep <a697133892@126.com>
2026-02-02 00:56:16 +0800

    Update 区间查询单点修改.cpp

--- 数据结构 /线段树/区间查询单点修改.cpp
+++ 数据结构 /线段树/区间查询单点修改.cpp
@@ -10,12 +10,14 @@ const int N = 2e6 + 10;
 
 // 线段树节点只需要存结果值
 struct info {
-    ll sum;
+    ll mn;
 };
 
 // 两个子节点的合并逻辑
-info operator+(const info &l, const info &r) {
-    return {l.sum + r.sum};
+info operator+(const info& l, const info& r) {
+    info res;
+    res.mn = min(l.mn, r.mn);
+    return res;
 }
 
 struct node {
@@ -24,19 +26,13 @@ struct node {
 
 ll a[N];
 
-// 辅助函数：左儿子和右儿子
 inline int ls(int x) { return x << 1; }
 inline int rs(int x) { return x << 1 | 1; }
 
-// 向上更新节点信息
-void update(int id) { 
-    seg[id].val = seg[ls(id)].val + seg[rs(id)].val; 
-}
-
-// 建树 [l, r]
+void update(int id) { seg[id].val = seg[ls(id)].val + seg[rs(id)].val; }
 void build(int id, int l, int r) {
     if (l == r) {
-        seg[id].val.sum = a[l];
+        seg[id].val.mn = a[l];
         return;
     }
     int mid = (l + r) >> 1;
@@ -45,10 +41,9 @@ void build(int id, int l, int r) {
     update(id);
 }
 
-// 单点修改：将 a[pos] 修改为 val
 void change(int id, int l, int r, int pos, ll val) {
     if (l == r) {
-        seg[id].val.sum = val; // 或者使用 += val，取决于你的需求
+        seg[id].val.mn = val;  
         return;
     }
     int mid = (l + r) >> 1;
@@ -56,42 +51,32 @@ void change(int id, int l, int r, int pos, ll val) {
         change(ls(id), l, mid, pos, val);
     else
         change(rs(id), mid + 1, r, pos, val);
-    update(id); // 修改后必须向上更新
+    update(id); 
 }
 
-// 区间查询：查询 [ql, qr] 的和
-ll qur(int id, int l, int r, int ql, int qr) {
-    if (ql <= l && r <= qr)
-        return seg[id].val.sum;
-    
+info qur(int id, int l, int r, int ql, int qr) {
+    if (ql <= l && r <= qr) return seg[id].val;
     int mid = (l + r) >> 1;
-    ll res = 0;
-    if (ql <= mid) res += qur(ls(id), l, mid, ql, qr);
-    if (qr > mid)  res += qur(rs(id), mid + 1, r, ql, qr);
-    
-    return res;
+    if (qr <= mid) {
+        return qur(ls(id), l, mid, ql, qr);
+    }
+    if (ql > mid) {
+        return qur(rs(id), mid + 1, r, ql, qr);
+    }
+    return qur(ls(id), l, mid, ql, qr) + qur(rs(id), mid + 1, r, ql, qr);
 }
 
 void solve() {
     int n, m;
-    if (!(cin >> n >> m)) return;
+    cin >> n >> m;
     rep(1, n) cin >> a[i];
-    
     build(1, 1, n);
-    
     while (m--) {
-        int op;
-        cin >> op;
-        if (op == 1) { // 单点修改
-            int pos;
-            ll val;
-            cin >> pos >> val;
-            change(1, 1, n, pos, val);
-        } else { // 区间查询
-            int x, y;
-            cin >> x >> y;
-            cout << qur(1, 1, n, x, y) << endl;
-        }
+        int x, y;
+        cin >> x >> y;
+        x++;            
+        if (x > y) swap(x, y); 
+        cout << qur(1, 1, n, x, y).mn << endl;
     }
 }
 
